ef5b14d83e91f6ec3cc22f3c7d1a497c41d8712d
Matt Reed <matthewreed02@gmail.com>
2024-04-12 12:38:59 -0700

    add script

---
+++ scripts/serialize_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#SBATCH --job-name=serialize_data
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=10:00:00
+#SBATCH --output=scripts/logs/serialize_data_%j.out
+#SBATCH --error=scripts/logs/serialize_data_%j.err
+#SBATCH --mem=100G
+
+# Optional: activate a conda environment to use for this job
+eval "$(conda shell.bash hook)"
+conda activate cs336_basics
+
+python data/data.py
