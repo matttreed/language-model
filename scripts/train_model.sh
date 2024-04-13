5dafff36cad4d9a590929ec1686c68b55eeb2919
Matt Reed <matthewreed02@gmail.com>
2024-04-13 16:35:41 -0700

    change device of mask

--- scripts/train_model.sh
+++ scripts/train_model.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=serialize_data
+#SBATCH --job-name=train_model
 #SBATCH --partition=batch
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --gpus=1
 #SBATCH --time=01:00:00
-#SBATCH --output=scripts/logs/serialize_data_%j.out
-#SBATCH --error=scripts/logs/serialize_data_%j.err
+#SBATCH --output=scripts/logs/train_model_%j.out
+#SBATCH --error=scripts/logs/train_model_%j.err
 #SBATCH --mem=100G
 
 # Optional: activate a conda environment to use for this job
