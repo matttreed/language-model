cee5f3f16266a5dca1411825fc72b7780097928b
Matt Reed <matthewreed02@gmail.com>
2024-04-13 22:56:46 -0700

    new scripts

---
+++ scripts/train_model_2.3.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=train_model
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --gpus=1
+#SBATCH --time=00:30:00
+#SBATCH --output=scripts/logs/train_model_%j.out
+#SBATCH --error=scripts/logs/train_model_%j.err
+#SBATCH --mem=100G
+
+# Optional: activate a conda environment to use for this job
+eval "$(conda shell.bash hook)"
+conda activate cs336_basics
+
+python src/main.py --version 2.3 --train
